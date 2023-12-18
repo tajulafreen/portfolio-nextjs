f5331cfbb178fe6a6823bc8325bca19619c33296
afreen shaik <affushaik9493@gmail.com>
2023-12-18 20:07:26 +0530

    add required icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,91 +1,15 @@
-export const Skill_data = [
-    {
-      skill_name: "Html 5",
-      Image: "/html.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Css",
-      Image: "/css.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Java Script",
-      Image: "/js.png",
-      width: 65,
-      height: 65,
-    },
-    {
-      skill_name: "Tailwind Css",
-      Image: "/tailwind.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "React",
-      Image: "/react.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Redux",
-      Image: "/redux.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "React Query",
-      Image: "/reactquery.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Type Script",
-      Image: "/ts.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Next js 13",
-      Image: "/next.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Framer Motion",
-      Image: "/framer.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Node js",
-      Image: "/node-js.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Mongo db",
-      Image: "/mongodb.png",
-      width: 40,
-      height: 40,
-    },
-  
-  ];
-  
   export const Socials = [
     {
-      name: "Discord",
-      src: "/discord.svg",
+      name: "LinkedIn",
+      src: "/linkedin.png",
     },
     {
-      name: "Facebook",
-      src: "/facebook.svg",
+      name: "Slack",
+      src: "/slack.png",
     },
     {
-      name: "Instagram",
-      src: "/instagram.svg",
+      name: "Github",
+      src: "/github.png",
     },
   ];
   
@@ -117,11 +41,11 @@ export const Skill_data = [
       height: 80,
     },
     {
-      skill_name: "Material UI",
-      Image: "/mui.png",
-      width: 80,
-      height: 80,
-    },
+        skill_name: "Bootstrap",
+        Image: "/boostrap.jpeg",
+        width: 80,
+        height: 80,
+      },
     {
       skill_name: "React",
       Image: "/react.png",
@@ -131,12 +55,6 @@ export const Skill_data = [
     {
       skill_name: "Redux",
       Image: "/redux.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "React Query",
-      Image: "/reactquery.png",
       width: 80,
       height: 80,
     },
@@ -156,16 +74,34 @@ export const Skill_data = [
   
   export const Backend_skill = [
     {
+        skill_name: "Material UI",
+        Image: "/mui.png",
+        width: 80,
+        height: 80,
+      },
+    {
       skill_name: "Node js",
       Image: "/node-js.png",
       width: 80,
       height: 80,
     },
     {
-      skill_name: "Mongo db",
-      Image: "/mongodb.png",
-      width: 40,
-      height: 40,
+        skill_name: "Github",
+        Image: "/github.png",
+        width: 80,
+        height: 80,
+      },
+    {
+      skill_name: "Git",
+      Image: "/git.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Ruby",
+      Image: "/ruby.webp",
+      width: 60,
+      height: 60,
     },
     {
       skill_name: "Postger SQL",
@@ -174,8 +110,8 @@ export const Skill_data = [
       height: 70,
     },
     {
-      skill_name: "My SQL",
-      Image: "/mysql.png",
+      skill_name: "Rails",
+      Image: "/rails.png",
       width: 70,
       height: 70,
     },
@@ -183,35 +119,23 @@ export const Skill_data = [
   
   export const Full_stack = [
     {
-      skill_name: "React Native",
-      Image: "/ReactNative .png",
-      width: 70,
-      height: 70,
-    },
-    {
       skill_name: "Docker",
       Image: "/docker.webp",
       width: 70,
       height: 70,
     },
-  
     {
-      skill_name: "Figma",
-      Image: "/figma.png",
-      width: 50,
-      height: 50,
-    },
-  
+        skill_name: "API's",
+        Image: "/api.jpeg",
+        width: 70,
+        height: 70,
+      },
+      {
+        skill_name: "Framer Motion",
+        Image: "/framer.png",
+        width: 80,
+        height: 80,
+    },  
   ];
   
-  
-  
-  export const Other_skill = [
-    {
-      skill_name: "Go",
-      Image: "/go.png",
-      width: 60,
-      height: 60,
-    },
-  ];
   
