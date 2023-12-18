15d397a579310ec7c2a748582db9691d226829e4
afreen shaik <affushaik9493@gmail.com>
2023-12-18 22:12:17 +0530

    Import Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/main/StarBackground'
 import Navbar from '@/components/main/Navbar'
+import Footer from '@/components/main/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +23,9 @@ export default function RootLayout({
       >
         <StarsCanvas />
         <Navbar />
-        {children}</body>
+        {children}
+        <Footer />
+        </body>
     </html>
   )
 }
