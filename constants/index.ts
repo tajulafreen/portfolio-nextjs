48ac92320f4949f3b5a18179b00d2b941132fd05
afreen shaik <affushaik9493@gmail.com>
2023-12-18 21:28:53 +0530

    add encrytion  theme

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,85 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/reactquery.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/framer.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/stripe.webp",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+
   export const Socials = [
     {
       name: "LinkedIn",
