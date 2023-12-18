1ec6f948ab08a784f549d8de23e1959664dea5e7
afreen shaik <affushaik9493@gmail.com>
2023-12-18 16:34:59 +0530

    import hero function

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Hero from '@/components/main/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='w-full h-full'>
-      <div className="flex flex-col gap-20"></div>
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
     </main>
   )
 }
