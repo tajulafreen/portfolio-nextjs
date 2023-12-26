1642947c5240c8ffd247e50e4a5f69eca9d1509f
afreen shaik <affushaik9493@gmail.com>
2023-12-26 10:12:42 +0530

    create function for fetching data

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+"use client"
+import React , { useEffect, useState } from "react";
 import ProjectCard from "../sub/ProjectCard";
-
+const Projects = () => {
 const projectDetails = [
   {
-    name: 'TO-DO-LIST',
+    name: 'Special Meals',
     description: [
 
       'A to-do list is a simple and effective organizational tool used to keep track of tasks, activities, or goals that need to be accomplished within a certain timeframe. It serves as a visual representation of the work that needs to be completed, helping individuals prioritize and manage their time more efficiently.',
       'To-do lists can be created using various mediums, such as paper and pen, electronic devices, or specialized productivity applications. Many digital tools offer additional features like due dates, reminders, and categorization options, which further enhance their functionality.',
     ],
-    image: './firstproject.png',
+    image: '/specialsmeals.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     live: 'https://tajulafreen.github.io/To-Do-List/dist/',
     source:
@@ -23,8 +24,8 @@ const projectDetails = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     ],
-    image: './Snapshoot Portfolio (2).png',
-    technologies: ['HTML', 'CSS', 'Tailwind'],
+    image: '/spacetravellers.png',
+    technologies: ['Tailwind', 'JavaScript','React','Api'],
     live: 'https://tajulafreen.github.io/Portfolio/',
     source:
       'https://github.com/tajulafreen/Portfolio/blob/main/index.html',
@@ -36,21 +37,21 @@ const projectDetails = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     ],
-    image: './Snapshoot Portfolio (2).png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
+    image: '/doctorappointment.png',
+    technologies: ['JavaScript','React','Ruby','Rails','Postgresql'],
     live: 'https://tajulafreen.github.io/Portfolio/',
     source:
       'https://github.com/tajulafreen/Portfolio/blob/main/index.html',
   },
   // Project #4
   {
-    name: 'Keeping track of hundreds of components #4',
+    name: 'Crytocurrency Track',
     description: [
       'This is the description of forth project',
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     ],
-    image: './Snapshoot Portfolio (2).png',
+    image: '/cryptocurrency.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     live: 'https://tajulafreen.github.io/Portfolio/',
     source:
@@ -58,65 +59,55 @@ const projectDetails = [
   },
   // Project #5
   {
-    name: 'Keeping track of hundreds of components #5',
+    name: 'MAMA Food',
     description: [
       'This is the description of fifth project',
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     ],
-    image: './Snapshoot Portfolio (2).png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
+    image: '/mamafood.png',
+    technologies:['HTML', 'CSS', 'JavaScript'],
     live: 'https://tajulafreen.github.io/Portfolio/',
     source:
       'https://github.com/tajulafreen/Portfolio/blob/main/index.html',
   },
   // Project #6
   {
-    name: 'Keeping track of hundreds of components #6',
+    name: 'Leader Board',
     description: [
       'This is the description of sixth project',
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
     ],
-    image: './Snapshoot Portfolio (2).png',
-    technologies: ['HTML', 'CSS', 'JavaScript'],
+    image: '/leaderboard.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
     live: 'https://tajulafreen.github.io/Portfolio/',
     source:
       'https://github.com/tajulafreen/Portfolio/blob/main/index.html',
   },
 ];
 
-const Projects = () => {
-  return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
-      </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+return (
+  <div className="flex flex-col items-center justify-center py-20" id="projects p-[5%]">
+    <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      My Projects
+    </h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
+      {projectDetails.map((project, index) => (
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          technologies=""
+          key={index}
+          src={project.image}
+          title={project.name}
+          description={project.description[0]}
+          technologies={Array.isArray(project.technologies) ? project.technologies.join(", ") : project.technologies}
+          source={project.source}
+          live={project.live}
         />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          technologies=""
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          technologies=""
-        />
-      </div>
+      ))}
     </div>
-  );
+  </div>
+);
 };
+
 
 export default Projects;
