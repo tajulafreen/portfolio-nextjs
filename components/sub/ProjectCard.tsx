20e833e24ae62c573a425bda28df9d0d8f992889
afreen shaik <affushaik9493@gmail.com>
2023-12-26 07:44:15 +0530

    add arguments

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,9 +5,10 @@ interface Props {
   src: string;
   title: string;
   description: string;
+  technologies: string
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description,technologies }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -21,6 +22,7 @@ const ProjectCard = ({ src, title, description }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
+        <div className="flex gap-2">{technologies}</div>
       </div>
     </div>
   );
