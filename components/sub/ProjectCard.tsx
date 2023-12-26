573011e1d303cfddb30afc69d614fcf5d838424a
afreen shaik <affushaik9493@gmail.com>
2023-12-26 10:12:17 +0530

    update props

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,12 +5,14 @@ interface Props {
   src: string;
   title: string;
   description: string;
-  technologies: string
+  technologies: string | string[]; // Update the type for technologies
+  source: string;
+  live: string;
 }
 
-const ProjectCard = ({ src, title, description,technologies }: Props) => {
+const ProjectCard = ({ src, title, description, technologies, source, live }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative  rounded-lg shadow-lg border border-[#2A0E61] ">
       <Image
         src={src}
         alt={title}
@@ -22,7 +24,11 @@ const ProjectCard = ({ src, title, description,technologies }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
-        <div className="flex gap-2">{technologies}</div>
+        <p>{Array.isArray(technologies) ? technologies.join(", ") : technologies}</p>
+        <ul className="flex justify-between">
+          <li><p>{source}</p></li>
+          <li><p>{live}</p></li>
+        </ul>
       </div>
     </div>
   );
