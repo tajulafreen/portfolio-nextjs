34f7c706ab8613fdf65a279662aa29fd1e21effd
afreen shaik <affushaik9493@gmail.com>
2023-12-28 05:14:08 +0530

    remove footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/main/StarBackground'
 import Navbar from '@/components/main/Navbar'
-import Footer from '@/components/main/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +24,6 @@ export default function RootLayout({
         <StarsCanvas />
         <Navbar />
         {children}
-        <Footer />
         </body>
     </html>
   )
