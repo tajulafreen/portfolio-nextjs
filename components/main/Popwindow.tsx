e12f9034b911c625fc75c65440ad9392f4d9ee57
afreen shaik <affushaik9493@gmail.com>
2023-12-27 09:57:26 +0530

    create a dunction to add modal

---
+++ components/main/Popwindow.tsx
@@ -0,0 +1,51 @@
+"use client"
+import React from 'react';
+import { projectDetails } from '@/constants/Project';
+import Image from 'next/image';
+
+interface ProjectProps {
+  project: {
+    name: string;
+    description: string[];
+    image: string;
+    technologies: string[];
+    live: string;
+    source: string;
+  };
+}
+
+const Project = ({ project }: ProjectProps) => {
+  return (
+    <div key={project.name}>
+      <Image src={project.image} alt={project.name} />
+      <h2>{project.name}</h2>
+      <ul>
+        {project.technologies.map((desc, index) => (
+          <li key={index}>{desc}</li>
+        ))}
+      </ul>
+      <ul>
+        {project.description.map((desc, index) => (
+          <li key={index}>{desc}</li>
+        ))}
+      </ul>
+      <p>Live: <a href={project.live}>{project.live}</a></p>
+      <p>Source: <a href={project.source}>{project.source}</a></p>
+      
+       
+    </div>
+  );
+}
+
+const Popwindow = () => {
+  return (
+    <div>
+      <h1>All Projects</h1>
+      {projectDetails.map((project, index) => (
+        <Project key={index} project={project} />
+      ))}
+    </div>
+  );
+}
+
+export default Popwindow;
