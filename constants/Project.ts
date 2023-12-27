a88ccf95a0881653e44606289f121a52144396c8
afreen shaik <affushaik9493@gmail.com>
2023-12-27 08:24:29 +0530

    edit data

---
+++ constants/Project.ts
@@ -0,0 +1,79 @@
+export const projectDetails = [
+    {
+      name: 'Special Meals',
+      description: [
+  
+        'Special Meals project is built using two APIs. The project allows users to view meals they like, like the meals and add comments.It uses an internal API to store the user interactions with the meals. The project is built using webpack and vanilla JavaScript.',
+        'The "Special Meals" project is a web application designed to showcase various meals while allowing users to engage by liking, favoriting, and commenting on these dishes. It integrates two APIs, one for retrieving meal data and another internal API to record user interactions. The platform enables users to explore a diverse range of meals, express their preferences by marking favorites, and share feedback through comments. Developed using Webpack and vanilla JavaScript, it offers an intuitive interface for users to interact with meals, fostering a user-friendly experience through simple yet effective functionalities like liking, favoriting, and leaving comments on different meal options.'
+    ],
+      image: '/specialsmeals.png',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      live: 'https://devawinner.github.io/special-meals/dist/',
+      source:
+        'https://github.com/tajulafreen/special-meals',
+    },
+    // Project #2
+    {
+      name: 'Space Travellers',
+      description: [
+        'A web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions. It is built with real live data from the SpaceX API.',
+        "The web application is tailored for a company specializing in commercial and scientific space travel services, providing users with the capability to reserve rockets and participate in specific space missions. Leveraging authentic data from the SpaceX API, users can access genuine information about rocket availability and mission details. The platform offers a seamless booking experience, enabling individuals to select and reserve rockets while granting them the opportunity to join and engage in curated space missions. With a foundation rooted in real-time data from the SpaceX API, this application presents an immersive and informative environment for users keen on exploring commercial and scientific space travel opportunities."
+    ],
+      image: '/spacetravellers.png',
+      technologies: ['Tailwind', 'JavaScript','React','Api'],
+      live: 'https://space-travelers-0cew.onrender.com/',
+      source:
+        'https://github.com/tajulafreen/Space-Travelers-HUB',
+    },
+    {
+      name: 'Doctor Appointment',
+      description: [
+        'Its an Online Doctor Reservation web application designed to provide users with the ability to reserve, view, and manage doctors and doctor appointment reservation . The system aims to streamline the process of finding and booking doctors from various locations',
+        "The Online Doctor Reservation web application offers users an intuitive platform to effortlessly manage doctor appointments and reservations. With its user-friendly interface, individuals can seamlessly view and book appointments with doctors across different locations. The system centralizes the process of finding and reserving appointments, providing a streamlined experience for users seeking medical consultations. By offering a comprehensive overview of available doctors and their schedules, the application simplifies the task of securing appointments, enhancing accessibility to healthcare services for individuals across diverse locations.",
+      ],
+      image: '/doctorappointment.png',
+      technologies: ['JavaScript','React','Ruby','Rails','Postgresql'],
+      live: 'https://doctor-appointment-eqlq.onrender.com/',
+      source:
+        'https://github.com/tajulafreen/Final-capstone-Frontend',
+    },
+    // Project #4
+    {
+      name: 'Crytocurrency The Future',
+      description: [
+        'Crypto the future Is a website that provides users with the standing of Crypto currencies with their latest statistics accumulated during the 2019-2020 season',
+        "Crypto the Future is a comprehensive website designed to furnish users with up-to-date information and statistics on various cryptocurrencies. It presents an overview of the performance and standings of cryptocurrencies, compiling the latest statistical data from the 2019-2020 season. The platform serves as a valuable resource for individuals interested in monitoring and analyzing the trends, market positions, and key statistics of different cryptocurrencies within the specified timeframe. Offering insights into the dynamic landscape of digital currencies, the website serves as a reference hub for users seeking comprehensive details and analysis on the cryptocurrency market during the specified period..",
+      ],
+      image: '/cryptocurrency.png',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      live: 'https://crypto-the-future.onrender.com/',
+      source:
+        'https://github.com/tajulafreen/CRYPTO-The-future/',
+    },
+    // Project #5
+    {
+      name: 'MAMA Food',
+      description: [
+        'MAMAfood a online learning food, Bring the most delicious food to your home. Mama s food gives an incredible opportunity to learn various cooking techniques.Give you Access to an ever-growing library of classes by the worlds best chefs.',
+        "MAMA food is an innovative online platform dedicated to providing an enriching culinary experience by delivering delicious and diverse cuisines directly to users  homes. Offering a unique fusion of learning and gastronomy, MAMA food presents an extensive array of cooking techniques, recipes, and culinary knowledge through its comprehensive online learning platform. By granting access to an expanding collection of classes curated by globally renowned chefs, it empowers users to master culinary skills, explore new recipes, and enhance their cooking prowess. With a focus on convenience and quality, MAMA food endeavors to bring the finest and most delectable dishes to individuals doorsteps while fostering a community of passionate food enthusiasts eager to embark on a journey of culinary discovery.",
+      ],
+      image: '/mamafood.png',
+      technologies:['HTML', 'CSS', 'JavaScript'],
+      live: 'https://tajulafreen.github.io/first-capstone-project/',
+      source:
+        'https://github.com/tajulafreen/MAMA-Food',
+    },
+    // Project #6
+    {
+      name: 'Leader Board',
+      description: [
+        'A leaderboard is a ranked list or table that displays the performance or scores of individuals or entities, typically in a competitive setting.',
+        "A leaderboard is a structured and often visualized representation showcasing the ranking, scores, or performance of individuals, entities, or participants within a specific competitive context. It provides a clear and organized layout presenting the relative standings of competitors, usually arranged in descending order based on their achievements, points, or accomplishments. Typically found in gaming, sports, business, or various online platforms, leaderboards offer users a quick and easily understandable overview of the top performers, fostering competition, motivation, and comparison among participants. This tool serves as a means of recognition, encouraging continued engagement and striving for higher positions within the displayed rankings.",
+      ],
+      image: '/leaderboard.png',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
+      live: 'https://github.com/tajulafreen/Leaderboard',
+      source:
+        'https://github.com/tajulafreen/Leaderboard',
+    },
+  ];
