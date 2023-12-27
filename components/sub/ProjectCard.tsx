04bb3ae95ee21f36d8f9e67064344ebc1e5ff195
afreen shaik <affushaik9493@gmail.com>
2023-12-28 03:37:54 +0530

    update project card

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,29 +1,35 @@
-import Image from "next/image";
-import React from "react";
+import React from 'react';
+import Image from 'next/image';
 
 interface Props {
   src: string;
   title: string;
   description: string;
+  onReadMoreClick: () => void;
 }
 
-const ProjectCard = ({ src, title, description}: Props) => {
+const ProjectCard = ({ src, title, description, onReadMoreClick }: Props) => {
   return (
-    <div className="flex flex-col relative  items-center overflow-hidden rounded-3xl shadow-lg border border-[#2A0E61] h-[500px] mx-[2%] ">
+    <div className="flex flex-col relative items-center overflow-hidden rounded-3xl shadow-lg border border-[#2A0E61] h-[500px] mx-[2%]">
       <Image
         src={src}
         alt={title}
         width={1000}
         height={1000}
-        className="h-[50%] object-contain w-[110%]  bg-opacity-10 "
+        className="h-[50%] object-contain w-[110%] bg-opacity-10"
       />
 
-      <div className=" pt-[-16px] mt-0 px-3 h-[40%]">
+      <div className="pt-[-16px] mt-0 px-3 h-[40%]">
         <h1 className="text-2xl font-semibold text-white text-center">{title}</h1>
-        <p className="mt-2 text-[14px]  text-gray-300">{description}</p>
-        
+        <p className="mt-2 text-[14px] text-gray-300">{description}</p>
       </div>
-      <button type="button"  className="bottom-3 h-[40px]  w-[150px] button-primary text-center text-white cursor-pointer rounded-xl items-center">read more</button>
+      <button
+        type="button"
+        onClick={onReadMoreClick}
+        className="bottom-3 h-[40px] w-[150px] button-primary text-center text-white cursor-pointer rounded-xl items-center"
+      >
+        read more
+      </button>
     </div>
   );
 };
