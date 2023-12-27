d2740feab0049fff0c18597572bf5a53e23ee82d
afreen shaik <affushaik9493@gmail.com>
2023-12-27 11:16:10 +0530

    update data

--- constants/Project.ts
+++ constants/Project.ts
@@ -3,7 +3,7 @@ export const projectDetails = [
       name: 'Special Meals',
       description: [
   
-        'Special Meals project is built using two APIs. The project allows users to view meals they like, like the meals and add comments.It uses an internal API to store the user interactions with the meals. The project is built using webpack and vanilla JavaScript.',
+        'Special Meals project is built using two APIs. The project allows users to view meals they like, like the meals and add comments.It uses an internal API to store the user interactions with the meals. The project is built using webpack....',
         'The "Special Meals" project is a web application designed to showcase various meals while allowing users to engage by liking, favoriting, and commenting on these dishes. It integrates two APIs, one for retrieving meal data and another internal API to record user interactions. The platform enables users to explore a diverse range of meals, express their preferences by marking favorites, and share feedback through comments. Developed using Webpack and vanilla JavaScript, it offers an intuitive interface for users to interact with meals, fostering a user-friendly experience through simple yet effective functionalities like liking, favoriting, and leaving comments on different meal options.'
     ],
       image: '/specialsmeals.png',
@@ -28,7 +28,7 @@ export const projectDetails = [
     {
       name: 'Doctor Appointment',
       description: [
-        'Its an Online Doctor Reservation web application designed to provide users with the ability to reserve, view, and manage doctors and doctor appointment reservation . The system aims to streamline the process of finding and booking doctors from various locations',
+        'Its an Online Doctor Reservation web application designed to provide users with the ability to reserve, view, and manage doctors and doctor appointment reservation . The system aims to streamline the process...',
         "The Online Doctor Reservation web application offers users an intuitive platform to effortlessly manage doctor appointments and reservations. With its user-friendly interface, individuals can seamlessly view and book appointments with doctors across different locations. The system centralizes the process of finding and reserving appointments, providing a streamlined experience for users seeking medical consultations. By offering a comprehensive overview of available doctors and their schedules, the application simplifies the task of securing appointments, enhancing accessibility to healthcare services for individuals across diverse locations.",
       ],
       image: '/doctorappointment.png',
