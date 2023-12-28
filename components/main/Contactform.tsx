f3b3a543c15c3fa9ba9cb132c7134bb4863e3e87
afreen shaik <affushaik9493@gmail.com>
2023-12-28 07:30:17 +0530

    create contact form

---
+++ components/main/Contactform.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+function Contactform() {
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+  return (
+    <div>Contactform</div>
+  )
+}
+
+export default Contactform
