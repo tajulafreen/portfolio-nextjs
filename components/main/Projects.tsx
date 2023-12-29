2fb5eec223a9194c1b6644975ad99a5653f5caf6
afreen shaik <affushaik9493@gmail.com>
2023-12-30 04:02:07 +0530

    add contact form

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,11 +1,12 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectCard from '../sub/ProjectCard';
 import { projectDetails } from '@/constants/Project';
-import Slider from 'react-slick'
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Popwindow from './Popwindow';
+import ContactForm from './Contactform';
 
 const Projects = () => {
   const [showPopUp, setShowPopUp] = useState(false);
@@ -19,12 +20,14 @@ const Projects = () => {
   const closePopUp = () => {
     setShowPopUp(false);
   };
-  
-  if (showPopUp) {
-    document.body.style.overflow = "hidden";
-  } else {
-    document.body.style.overflow = "auto";
-  }
+
+  useEffect(() => {
+    if (showPopUp) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [showPopUp]);
 
   const settings = {
     accessibility: true,
@@ -63,30 +66,31 @@ const Projects = () => {
 
   return (
     <div className='z-[20]'>
-    <div className="p-20 " id="projects">
-      <h1 className="text-[40px] text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
-      </h1>
-      <div className="gap-20">
-        <Slider {...settings}>
-          {projectDetails.map((project, index) => (
-            <ProjectCard
-              key={index}
-              src={project.image}
-              title={project.name}
-              description={project.description[0]}
-              onReadMoreClick={() => togglePopUp(project)}
-            />
-          ))}
-        </Slider>
+      <div className="p-20 " id="projects">
+        <h1 className="text-[40px] text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+          My Projects
+        </h1>
+        <div className="gap-20">
+          <Slider {...settings}>
+            {projectDetails.map((project, index) => (
+              <ProjectCard
+                key={index}
+                src={project.image}
+                title={project.name}
+                description={project.description[0]}
+                onReadMoreClick={() => togglePopUp(project)}
+              />
+            ))}
+          </Slider>
+        </div>
+        {showPopUp && (
+          <Popwindow
+            project={selectedProject}
+            onClosePopUp={closePopUp}
+          />
+        )}
       </div>
-      {showPopUp && (
-        <Popwindow
-          project={selectedProject}
-          onClosePopUp={closePopUp}
-        />
-      )}
-      </div>
+        <ContactForm visible={showPopUp}/>
     </div>
   );
 };
