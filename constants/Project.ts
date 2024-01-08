bb664627c933a6003b5f9f50060f1281741e1b27
afreen shaik <affushaik9493@gmail.com>
2024-01-09 04:05:21 +0530

    corrected the grammers mistakes

--- constants/Project.ts
+++ constants/Project.ts
@@ -3,7 +3,7 @@ export const projectDetails = [
       name: 'Special Meals',
       description: [
   
-        'Special Meals project is built using two APIs. The project allows users to view meals they like, like the meals and add comments.It uses an internal API to store the user interactions with the meals. The project is built using webpack.',
+        'Special Meals project is built using two APIs. The project allows users to view meals they like, like the meals, and add comments. It uses an internal API to store the user interactions with the meals. The project is built using webpack.',
         'The "Special Meals" project is a web application designed to showcase various meals while allowing users to engage by liking, favoriting, and commenting on these dishes. It integrates two APIs, one for retrieving meal data and another internal API to record user interactions. The platform enables users to explore a diverse range of meals, express their preferences by marking favorites, and share feedback through comments. Developed using Webpack and vanilla JavaScript, it offers an intuitive interface for users to interact with meals, fostering a user-friendly experience through simple yet effective functionalities like liking, favoriting, and leaving comments on different meal options.'
     ],
       image: '/specialsmeals.png',
@@ -17,8 +17,8 @@ export const projectDetails = [
       name: 'Space Travellers',
       description: [
         'A web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions. It is built with real live data from the SpaceX API.',
-        "The web application is tailored for a company specializing in commercial and scientific space travel services, providing users with the capability to reserve rockets and participate in specific space missions. Leveraging authentic data from the SpaceX API, users can access genuine information about rocket availability and mission details. The platform offers a seamless booking experience, enabling individuals to select and reserve rockets while granting them the opportunity to join and engage in curated space missions. With a foundation rooted in real-time data from the SpaceX API, this application presents an immersive and informative environment for users keen on exploring commercial and scientific space travel opportunities."
-    ],
+        "The web application is tailored for a company specializing in commercial and scientific space travel services, providing users with the capability to reserve rockets and participate in specific space missions. Leveraging authentic data from the SpaceX API, users can access genuine information about rocket availability and mission details. The platform offers a seamless booking experience, enabling individuals to select and reserve rockets while granting them the opportunity to join and engage in curated space missions. With a foundation rooted in real-time data from the SpaceX API, this application presents an immersive and informative environment for users keen on exploring commercial and scientific space travel opportunities.",
+      ],
       image: '/spacetravellers.png',
       technologies: ['Tailwind', 'JavaScript','React','Api'],
       live: 'https://space-travelers-0cew.onrender.com/',
@@ -28,7 +28,7 @@ export const projectDetails = [
     {
       name: 'Doctor Appointment',
       description: [
-        'Its an Online Doctor Reservation web application designed to provide users with the ability to reserve, view, and manage doctors and doctor appointment reservation . The system aims to streamline the process.',
+        "It is an online Doctor Reservation web application designed to provide users with the ability to reserve, view, and manage doctor and doctor appointment reservations. The system aims to streamline the process.",
         "The Online Doctor Reservation web application offers users an intuitive platform to effortlessly manage doctor appointments and reservations. With its user-friendly interface, individuals can seamlessly view and book appointments with doctors across different locations. The system centralizes the process of finding and reserving appointments, providing a streamlined experience for users seeking medical consultations. By offering a comprehensive overview of available doctors and their schedules, the application simplifies the task of securing appointments, enhancing accessibility to healthcare services for individuals across diverse locations.",
       ],
       image: '/doctorappointment.png',
@@ -41,8 +41,8 @@ export const projectDetails = [
     {
       name: 'Crytocurrency The Future',
       description: [
-        'Crypto the future Is a website that provides users with the standing of Crypto currencies with their latest statistics accumulated during the 2019-2020 season.',
-        "Crypto the Future is a comprehensive website designed to furnish users with up-to-date information and statistics on various cryptocurrencies. It presents an overview of the performance and standings of cryptocurrencies, compiling the latest statistical data from the 2019-2020 season. The platform serves as a valuable resource for individuals interested in monitoring and analyzing the trends, market positions, and key statistics of different cryptocurrencies within the specified timeframe. Offering insights into the dynamic landscape of digital currencies, the website serves as a reference hub for users seeking comprehensive details and analysis on the cryptocurrency market during the specified period..",
+        'Crypto the Future is a website that provides users with the standing of Cryptocurrencies with their latest statistics accumulated during the 2019-2020 season.',
+        "Crypto the Future is a comprehensive website designed to furnish users with up-to-date information and statistics on various cryptocurrencies. It presents an overview of the performance and standings of cryptocurrencies, compiling the latest statistical data from the 2019-2020 season. The platform serves as a valuable resource for individuals interested in monitoring and analyzing the trends, market positions, and key statistics of different cryptocurrencies within the specified timeframe. Offering insights into the dynamic landscape of digital currencies, the website serves as a reference hub for users seeking comprehensive details and analysis on the cryptocurrency market during the specified period.",
       ],
       image: '/cryptocurrency.png',
       technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -54,8 +54,8 @@ export const projectDetails = [
     {
       name: 'MAMA Food',
       description: [
-        'MAMAfood a online learning food, Bring the most delicious food to your home. Mama s food gives an incredible opportunity to learn various cooking techniques.Give you Access to an ever-growing library of classes by the worlds best chefs.',
-        "MAMA food is an innovative online platform dedicated to providing an enriching culinary experience by delivering delicious and diverse cuisines directly to users  homes. Offering a unique fusion of learning and gastronomy, MAMA food presents an extensive array of cooking techniques, recipes, and culinary knowledge through its comprehensive online learning platform. By granting access to an expanding collection of classes curated by globally renowned chefs, it empowers users to master culinary skills, explore new recipes, and enhance their cooking prowess. With a focus on convenience and quality, MAMA food endeavors to bring the finest and most delectable dishes to individuals doorsteps while fostering a community of passionate food enthusiasts eager to embark on a journey of culinary discovery.",
+        "MAMA's food an online learning food, Bring the most delicious food to your home. Mama's food gives an incredible opportunity to learn various cooking techniques. Give you Access to an ever-growing library of classes by the world's best chefs.",
+        "MAMA's food is an innovative online platform dedicated to providing an enriching culinary experience by delivering delicious and diverse cuisines directly to users' homes. Offering a unique fusion of learning and gastronomy, MAMA's Food presents an extensive array of cooking techniques, recipes, and culinary knowledge through its comprehensive online learning platform. By granting access to an expanding collection of classes curated by globally renowned chefs, it empowers users to master culinary skills, explore new recipes, and enhance their cooking prowess. With a focus on convenience and quality, MAMA Food endeavors to bring the finest and most delectable dishes to individuals' doorsteps while fostering a community of passionate food enthusiasts eager to embark on a journey of culinary discovery.",
       ],
       image: '/mamafood.png',
       technologies:['HTML', 'CSS', 'JavaScript'],
@@ -79,8 +79,8 @@ export const projectDetails = [
     {
       name: 'To-DO List',
       description: [
-        'React Todo App is the app offers a seamless experience for managing todos effectively. Its user-friendly interface combined with powerful features like adding, removing, editing todos and change the position of todos.',
-        "The React Todo App is a user-centric application designed to streamline and organize tasks efficiently. Its intuitive interface provides users with a seamless experience for managing their to-do lists. The app offers essential functionalities such as adding new tasks, removing existing ones, and editing task details as needed. Additionally, users can easily rearrange the position of their tasks, allowing for prioritization and customization according to their preferences. With a focus on user-friendliness and practicality, this app simplifies task management by combining powerful features with a clean and accessible design, enhancing productivity and task organization for its users.",
+        'React Todo App is an app that offers a seamless experience for managing todos effectively. Its user-friendly interface is combined with powerful features like adding, removing, editing todos, and changing the position of todos.',
+        "The React Todo App is a user-centric application designed to streamline and organize tasks efficiently. Its intuitive interface provides users a seamless experience for managing their to-do lists. The app offers essential functionalities such as adding new tasks, removing existing ones, and editing task details as needed. Additionally, users can easily rearrange the position of their tasks, allowing for prioritization and customization according to their preferences. With a focus on user-friendliness and practicality, this app simplifies task management by combining powerful features with a clean and accessible design, enhancing user productivity and task organization.",
       ],
       image: '/todolist.png',
       technologies: ['HTML', 'CSS', 'JavaScript', 'React','Tailwind'],
